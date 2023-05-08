0296056374a7ce03d30a93251c8a8c6ea7cd271e
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2023-05-08 01:01:06 -0300

    first commit

---
+++ front/clint/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>
+    <header className="p-4 flex justify-between">
+      <a href="" className='flex items-center gap-2'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 -rotate-90">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>
+        <span className='font-bold text-xl'>Car Rental</span>
+      </a>
+      <div className='flex border border-gray-300 rounded-full py-3 px-6'>
+        <div>Anywhere</div>
+        <div className="border border-l border-gray-300"></div>
+        <div>Any week</div>
+        <div>Add guests</div>
+        <button>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+          </svg>
+        </button>
+      </div>
+    </header>
+   </div>
+  )
+}
+
+export default App
